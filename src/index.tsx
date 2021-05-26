59a9b378f4f82fca20d9d3c0e853ce1d602928f7
Cyle Ven Dawson <dawsonc623@gmail.com>
2021-05-26 09:05:50 -0500

    feat: set up initial React app

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React    from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div style={{color: "white"}}>Hello World!</div>,
+  document.getElementById(
+    "app"
+  )
+);
